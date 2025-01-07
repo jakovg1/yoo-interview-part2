863896f0b3d64d508f51308a9b5a18fff4441c05
Jakov <jakov.gracin1@gmail.com>
2025-01-07 14:02:24 +0100

    Add search

--- yoo-part2/src/app/main/main.component.ts
+++ yoo-part2/src/app/main/main.component.ts
@@ -1,4 +1,10 @@
-import { Component, signal, WritableSignal } from '@angular/core';
+import {
+  Component,
+  computed,
+  Signal,
+  signal,
+  WritableSignal,
+} from '@angular/core';
 import { DetailsComponent } from '../details/details.component';
 import { CommonModule } from '@angular/common';
 import { PhotosService } from '../photos.service';
@@ -6,17 +12,39 @@ import { Photo } from '../types';
 import { map } from 'rxjs';
 import { Router } from '@angular/router';
 import { ImageWrapperComponent } from '../image-wrapper/image-wrapper.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [DetailsComponent, CommonModule, ImageWrapperComponent],
+  imports: [
+    DetailsComponent,
+    CommonModule,
+    ImageWrapperComponent,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatProgressSpinnerModule,
+  ],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss',
 })
 export class MainComponent {
   public photos: WritableSignal<Photo[]> = signal([] as Photo[]);
   public activePhoto: Photo = {} as Photo;
+  public isLoading: boolean = true;
+
+  public filterControl: FormControl = new FormControl('');
+  public filterSignal = toSignal(this.filterControl.valueChanges);
+
+  public displayedPhotos: Signal<Photo[]> = computed(() => {
+    const filter = this.filterSignal() || '';
+    return this.photos().filter((photo) => photo.title.includes(filter));
+  });
 
   constructor(private photosService: PhotosService, private router: Router) {
     this.getPhotos();
@@ -35,6 +63,7 @@ export class MainComponent {
       )
       .subscribe((photos) => {
         this.photos.set(photos);
+        this.isLoading = false;
       });
   }
 
